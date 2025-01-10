a8459e0030bec8eb1322cbb8a22c14d2f9df8e22
ajeng-asa <ajengsetiy@gmail.com>
2025-01-10 10:35:03 +0700

    Add files via upload

---
+++ variabel.js
@@ -0,0 +1,50 @@
+let nama = "Ajeng setiyo asih" ;
+console.log(nama);
+
+let alamat = " loireng,jogo 2";
+console.log(alamat);
+
+let usia = "16 Tahun";
+console.log(usia);
+
+
+let TTL = "Demak 21 Januari 2009";
+console.log(TTL);
+
+let gender = "perempuan";
+console.log(gender);
+
+let agama = "islam";
+console.log(agama);
+
+let email = "ajengsetiy@gmail.com";
+console.log(email);
+
+let beratbadan = "45 kg";
+console.log(beratbadan);
+
+let tinggibadan = "151 cm";
+console.log(tinggibadan);
+
+let hobi = "menonton tv, menyanyi";
+console.log(hobi);
+
+let cita = "menjadi orang sukses + pengusaha muda";
+console.log(cita);
+
+let status = "pelajar";
+console.log(status);
+
+let sekolah = "smk n 1 sayung";
+console.log(sekolah);
+
+
+let jurusan = "pplg";
+console.log(jurusan);
+
+document.write("nama saya adalah"+ nama + "alamat saya di" + alamat + "tempat tanggal lahir saya" + TTL + "usia saya"
+    + usia + "gender saya" + gender + "agama saya" + agama + "email saya" + email + "beratbadan saya" + beratbadan +
+     "tinggibadan saya" + tinggibadan + "hobi saya" + hobi + "cita cita saya" + cita + "status" + status + 
+     "saya sekolah di" + sekolah + "dan jurusan saya adalah" + jurusan)
+
+
